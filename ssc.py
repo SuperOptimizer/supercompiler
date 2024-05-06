31e624e0687298e2567eee8d699a65ce529606d2
verditelabs <156155735+SuperOptimizer@users.noreply.github.com>
2024-05-06 14:13:54 -0500

    lightning updates

--- ssc.py
+++ ssc.py
@@ -1,37 +1,30 @@
-#Superoptimizing Super Compiler
-
 import os
-import ast
-import gzip
 import torch
-import torch.nn as nn
-import torch.optim as optim
 import tqdm
-from torch.utils.data import DataLoader
 import tarfile
 import random
 from x_transformers import XTransformer
-
-
+from collections import defaultdict
+from torch.utils.data import Dataset, DataLoader
+import torch.cuda.amp as amp
+import multiprocessing
 from util import report_cuda_size, report_model_size
-
-
-
-
+import pytorch_lightning as pl
+from lightning.pytorch.strategies import DeepSpeedStrategy
+from deepspeed.ops.adam import DeepSpeedCPUAdam
 if not torch.cuda.is_available():
-    raise RuntimeError('CUDA is not available')
-torch.set_default_device('cuda')
+    raise Exception("cuda not available")
 
 DEVICE = 'cuda'
 ROOTDIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 NUM_TOKENS = 261
-ENC_SEQ_LEN = 2048
-DEC_SEQ_LEN = 2048
-NUM_EPOCHS = 10
+ENC_SEQ_LEN = 4096
+DEC_SEQ_LEN = 4096
 BATCH_SIZE = 1
 LEARNING_RATE = 1e-4
 CHECKPOINT_EVERY = 1000
-GENERATE_EVERY = 1000
+GENERATE_EVERY = 10000
+NUM_EPOCHS = 10000
 
 ENCSTART, ENCEND, DECSTART, DECEND, PAD = [256, 257, 258, 259, 260]
 
@@ -48,127 +41,153 @@ def detokenize(tokens: [int]):
             ret.append(t)
     return bytes(ret)
 
+class XTransformerDataset(Dataset):
+    def __init__(self, rootdir):
+        self.rootdir = rootdir
+        self.tars = list(sorted(os.listdir(f'{rootdir}/data/')))[:10]
 
+        self.training_data = []
+        self.load_data()
 
-def getdata(batch_size, training_data, tars):
-    if len(training_data) < batch_size:
+    def __len__(self):
+        return len(self.training_data)
 
-        tar = random.choice(tars)
-        tars.remove(tar)
-        if len(tars) == 0:
-            tars = os.listdir(f'{ROOTDIR}/data/')
-        opt_objs = []
-        unopt_objs = []
-        training_data = []
-        if not tar.endswith('.tar.gz'):
-            raise ValueError('only .tar.gz training data is allowed under data/')
-        print(f"loading {ROOTDIR}/data/{tar}")
-        with tarfile.open(f"{ROOTDIR}/data/{tar}", 'r:gz') as tar:
-            # Get a list of file names in the archive
-            file_names = tar.getnames()
-            # Read a specific file from the archive
-            for member in tar.getmembers():
-                if member.name.endswith('.opt.cpp.o'):
-                    file_obj = tar.extractfile(member)
-                    content = file_obj.read()
-                    opt_objs.append(content)
-                elif member.name.endswith('.unopt.cpp.o'):
-                    file_obj = tar.extractfile(member)
-                    content = file_obj.read()
-                    unopt_objs.append(content)
-            if len(opt_objs) != len(unopt_objs):
-                raise RuntimeError(f'Mismatch in length of opt_objs and unopt_objs')
-            for unopt,opt in zip(unopt_objs, opt_objs):
-                unopt_tokens = tokenize(unopt)
-                opt_tokens = tokenize(opt)
-                if len(unopt_tokens) >= ENC_SEQ_LEN or len(opt_tokens) >= DEC_SEQ_LEN:
+    def __getitem__(self, idx):
+        src, tgt, mask = self.training_data[idx]
+        src = torch.tensor(src, dtype=torch.long)
+        tgt = torch.tensor(tgt, dtype=torch.long)
+        mask = torch.tensor(mask, dtype=torch.bool)
+        return src, tgt, mask
+
+    def load_data(self):
+        while self.tars:
+            tar = random.choice(self.tars)
+            self.tars.remove(tar)
+
+            if not tar.endswith('.tar.gz'):
+                continue
+
+            opt_objs = []
+            unopt_objs = []
+            print(f"loading {self.rootdir}/data/{tar}")
+            with tarfile.open(f"{self.rootdir}/data/{tar}", 'r:gz') as tar:
+                for member in tar.getmembers():
+                    if not member.name.endswith('.o'):
+                        continue
+
+                    if member.name.endswith('.opt.cpp.o'):
+                        file_obj = tar.extractfile(member)
+                        content = file_obj.read()
+                        opt_objs.append(content)
+                    elif member.name.endswith('.unopt.cpp.o'):
+                        file_obj = tar.extractfile(member)
+                        content = file_obj.read()
+                        unopt_objs.append(content)
+
+                if len(opt_objs) != len(unopt_objs):
+                    print("WARNING: Mismatch in length of opt_objs and unopt_objs.")
                     continue
-                unopt_tokens.insert(0,ENCSTART)
-                unopt_tokens.append(ENCEND)
-                opt_tokens.insert(0, DECSTART)
-                opt_tokens.append(DECEND)
-                mask = [True] * len(unopt_tokens)
-                mask.extend([False] * (ENC_SEQ_LEN - len(unopt_tokens)))
-                unopt_tokens.extend([PAD] * (ENC_SEQ_LEN - len(unopt_tokens)))
-                opt_tokens.extend([PAD] * (DEC_SEQ_LEN - len(opt_tokens)))
-                training_data.append([unopt_tokens, opt_tokens, mask])
-    while len(training_data) >= batch_size:
-        batch = training_data[:batch_size]
-        training_data = training_data[batch_size:]
-        mysrc = torch.tensor(list(x[0] for x in batch)).long().to(DEVICE)
-        mytgt = torch.tensor(list(x[1] for x in batch)).long().to(DEVICE)
-        mysrc_mask = torch.tensor(list(x[2] for x in batch)).bool().to(DEVICE)
-        return mysrc, mysrc_mask, mytgt, training_data, tars
-    assert False
 
+                for unopt, opt in zip(unopt_objs, opt_objs):
+                    unopt_tokens = tokenize(unopt)
+                    opt_tokens = tokenize(opt)
 
-def train(rank):
-  model = XTransformer(
-    dim=512,
-    pad_value=PAD,
-    tie_token_emb=True,
-    enc_attn_flash=True,
-    dec_attn_flash=True,
-    return_tgt_loss=True,
-    enc_num_tokens=NUM_TOKENS,
-    enc_depth=4,
-    enc_heads=4,
-    enc_max_seq_len=ENC_SEQ_LEN,
-    dec_num_tokens=NUM_TOKENS,
-    dec_depth=4,
-    dec_heads=4,
-    dec_max_seq_len=DEC_SEQ_LEN)
-  model = torch.compile(model)
-  report_model_size(model)
+                    if len(unopt_tokens) >= ENC_SEQ_LEN or len(opt_tokens) >= DEC_SEQ_LEN:
+                        #print(f"skip len unopt {len(unopt_tokens)} opt {len(opt_tokens)}")
+                        continue
 
-  optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-  if DEVICE == 'cuda':
-    scaler = torch.cuda.amp.GradScaler()
-  scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optim,T_0=100)
+                    unopt_tokens.insert(0, ENCSTART)
+                    unopt_tokens.append(ENCEND)
+                    opt_tokens.insert(0, DECSTART)
+                    opt_tokens.append(DECEND)
+                    mask = [True] * len(unopt_tokens)
+                    mask.extend([False] * (ENC_SEQ_LEN - len(unopt_tokens)))
+                    unopt_tokens.extend([PAD] * (ENC_SEQ_LEN - len(unopt_tokens)))
+                    opt_tokens.extend([PAD] * (DEC_SEQ_LEN - len(opt_tokens)))
+                    self.training_data.append((unopt_tokens, opt_tokens, mask))
 
-  training_data = []
-  tars = os.listdir(f'{ROOTDIR}/data')
+class XTransformerModule(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = XTransformer(
+            dim=1024,
+            pad_value=PAD,
+            tie_token_emb=True,
+            enc_attn_flash=True,
+            dec_attn_flash=True,
+            return_tgt_loss=True,
+            enc_num_tokens=NUM_TOKENS,
+            enc_depth=8,
+            enc_heads=8,
+            enc_max_seq_len=ENC_SEQ_LEN,
+            dec_num_tokens=NUM_TOKENS,
+            dec_depth=8,
+            dec_heads=8,
+            dec_max_seq_len=DEC_SEQ_LEN
+        )
+        self.model = torch.compile(self.model)
 
-  for i in tqdm.tqdm(range(5000), mininterval=10., desc='training'):
-    model.train()
-    optim.zero_grad()
+    def forward(self, src, tgt, mask):
+        return self.model(src, tgt, mask=mask)
 
-    src, src_mask, tgt, training_data, tars = getdata(BATCH_SIZE, training_data, tars)
-    if DEVICE == 'cuda':
-      with torch.cuda.amp.autocast(dtype=torch.bfloat16):
-        loss = model(src, tgt, mask=src_mask)
-      scaler.scale(loss).backward()
-      scaler.step(optim)
-      scaler.update()
-    else:
-      loss = model(src, tgt, mask=src_mask)
-      loss.backward()
-      optim.step()
-    scheduler.step(i/1)
-    print(f'{i}: {loss.item()}')
+    def training_step(self, batch, batch_idx):
+        src, tgt, mask = batch
+        src, tgt, mask = src.to(self.device), tgt.to(self.device), mask.to(self.device)
+        loss = self(src, tgt, mask)
+        self.log('train_loss', loss)
+        return loss
 
-    if i == 0 and DEVICE == 'cuda':
-      pass
-      #report_cuda_size()
-    if i % GENERATE_EVERY == 0:
-        report_cuda_size()
-        model.eval()
-        src, src_mask, tgt, training_data, tars = getdata(BATCH_SIZE, training_data, tars)
-        src, src_mask, tgt  = src[:1], src_mask[:1], tgt[:1]
-        start_tokens = torch.tensor([DECSTART]).to(DEVICE)
-        sample = model.generate(src, start_tokens, DEC_SEQ_LEN, mask = src_mask)
+    def validation_step(self, batch, batch_idx):
+        src, tgt, mask = batch
+        src, tgt, mask = src.to(self.device), tgt.to(self.device), mask.to(self.device)
+        loss = self(src, tgt, mask)
+        self.log('val_loss', loss)
 
-        print_stmt = f'\nRANK: {rank} start\n'
-        print_stmt += f"\ninput tokenized:  \n{detokenize(src.tolist()[0])} \n"
-        print_stmt += f"\npredicted detokenized:  \n{detokenize(sample.tolist())}\n"
-        print_stmt += f"\nactual detokenized:     \n{detokenize(tgt.tolist()[0])}\n"
-        print_stmt += f'\nRANK: {rank} end\n'
-        print(print_stmt)
+    def configure_optimizers(self):
+        # DeepSpeedCPUAdam provides 5x to 7x speedup over torch.optim.adam(w)
+        return DeepSpeedCPUAdam(self.parameters())
 
+    def generate(self, src, start_tokens, seq_len, mask):
+        return self.model.generate(src, start_tokens, seq_len, mask=mask)
+
+    def val_dataloader(self):
+        val_dataset = XTransformerDataset(ROOTDIR)
+        return DataLoader(val_dataset, batch_size=BATCH_SIZE, num_workers=multiprocessing.cpu_count())
 
 # Main function
 def main():
-    train(0)
+    dataset = XTransformerDataset(ROOTDIR)
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, num_workers=multiprocessing.cpu_count())
+
+
+    model = XTransformerModule()
+    trainer = pl.Trainer(
+        max_epochs=NUM_EPOCHS,
+        accelerator='gpu' if DEVICE == 'cuda' else 'cpu',
+        devices=2,
+        strategy="deepspeed_stage_3_offload",
+        precision="bf16-mixed",
+        callbacks=[
+            pl.callbacks.ModelCheckpoint(monitor='val_loss', dirpath='checkpoints', save_top_k=1, mode='min'),
+            pl.callbacks.LearningRateMonitor(logging_interval='epoch'),
+            pl.callbacks.DeviceStatsMonitor(False),
+        ]
+    )
+
+    trainer.fit(model, dataloader, val_dataloaders=model.val_dataloader())
+
+    # Generate samples after training
+    # Generate samples after training
+    #for batch in dataloader:
+    #    src, tgt, mask = batch
+    #    src, tgt, mask = src.to(DEVICE), tgt.to(DEVICE), mask.to(DEVICE)
+    #    start_tokens = torch.tensor([DECSTART]).to(DEVICE)
+    #    sample = model.generate(src, start_tokens, DEC_SEQ_LEN, mask=mask)
+    #    print_stmt = f"\ninput tokenized:  \n{detokenize(src.tolist()[0])} \n"
+    #    print_stmt += f"\npredicted detokenized:  \n{detokenize(sample.tolist())}\n"
+    #    print_stmt += f"\nactual detokenized:     \n{detokenize(tgt.tolist()[0])}\n"
+    #    print(print_stmt)
 
 if __name__ == '__main__':
+    torch.set_float32_matmul_precision('medium')
     main()
