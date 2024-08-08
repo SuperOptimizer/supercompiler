3b898ee253f11bbad0c9b57d09977e114148b2f9
verditelabs <156155735+SuperOptimizer@users.noreply.github.com>
2024-08-08 14:17:58 -0500

    working example

--- codegen.py
+++ codegen.py
@@ -1,151 +1,66 @@
 import multiprocessing
-from  subprocess import PIPE, run
-import os
+from subprocess import PIPE, run
 import platform
-import multiprocessing.dummy
+import os
+from concurrent.futures import ProcessPoolExecutor
+from util import randstring, ROOTDIR, TMP
 import tarfile
 import shutil
-import heapq
-from collections import defaultdict
-import os
-import glob
-from collections import defaultdict
-import heapq
-import base64
-import tokenizers
-from pathlib import Path
 
 
-from util import randstring, chunkify
-from util import ROOTDIR, TMP, HOMEDIR
-
-CCFLAGS = '-Wall -fcf-protection=none -fno-asynchronous-unwind-tables -fno-unwind-tables -march=znver4 '
-
-def gen_yarpgen(_):
-  path = f'{TMP}/{randstring(32)}'
-  os.makedirs(path, exist_ok=False)
-  ret = run(f'{ROOTDIR}/bin/{platform.system()}/yarpgen --std=c++ -o {path}'.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-  return f"{path}/func.cpp"
-
-def gen_csmith(_):
-  ret = run(f'{ROOTDIR}/bin/{platform.system()}/csmith --concise --max-funcs 1 --no-safe-math --nomain'.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-  prog = ret.stdout.decode('utf-8')
-  path = f'{TMP}/{randstring(32)}.o'
-  with open(path,'wt') as f:
-    f.write(prog)
-  return path
-
-def gen_ldrgen(_):
-  ret = run(f'/{HOMEDIR}/.opam/4.14.1/bin/frama-c -ldrgen -ldrgen-int-only'.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-  prog = ret.stdout.decode('utf-8')
-  path = f'{TMP}/{randstring(32)}.o'
-  with open(path,'wt') as f:
-    f.write(prog)
-  return path
-
-def gen_ccg(_):
-  ret = run(f'/{ROOTDIR}/bin/{platform.system()}/ccg --max-function 1 --max-localvars 4 --max-function-parameters 8 --min-statements-per-block 1 --max-statements-per-block 4 --max-expression-nesting 4 --max-block-nesting 4'.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-  prog = ret.stdout.decode('utf-8')
-  path = f'{TMP}/{randstring(32)}.o'
-  with open(path,'wt') as f:
-    f.write(prog)
-  return path
-
-def compile(path, objpath, opt):
-  clang = f'clang++-18 -xc++ -stdlib=libc++ ' if path.endswith('.cpp') else f'clang -xc '
-  clangret = run(f'{clang} -c {path} -o {objpath} {opt} {CCFLAGS} -include stdint.h'.split(), stdout=PIPE, stderr=PIPE)
-  stripret = run(f'llvm-strip-18 {objpath}'.split(), stdout=PIPE, stderr=PIPE)
-  objcopyret = run(f'llvm-objcopy-18 --remove-section .eh_frame --remove-section .note.GNU-stack --remove-section .comment --remove-section .llvm_addrsig {objpath}'.split(), stdout=PIPE, stderr=PIPE)
-  return f"{path}.o"
-
-def compile_unopt(path):
-  ext = ".unopt" + ('.cpp' if path.endswith('.cpp') else '.c')
-  return compile(path, f"{path}{ext}.o", "-O0")
-
-def compile_opt(path):
-  ext = ".opt" + ('.cpp' if path.endswith('.cpp') else '.c')
-  return compile(path, f"{path}{ext}.o", "-O3")
-
-def generate_code():
-  shutil.rmtree(TMP, ignore_errors=True)
-  ncpu = multiprocessing.cpu_count()
-  numtars = 10000
-  numruns = 20
-  numexisting = len(os.listdir(f"{ROOTDIR}/data"))
-  for i in range(numtars):
-    print(f"tar {i+numexisting}")
-    with multiprocessing.dummy.Pool(ncpu) as p:
-      ret = p.map(gen_yarpgen, list(range(ncpu*numruns)))
-    with multiprocessing.dummy.Pool(ncpu) as p:
-      unopt = p.map(compile_unopt, ret)
-    with multiprocessing.dummy.Pool(ncpu) as p:
-      opt = p.map(compile_opt, ret)
-    dirnum = 0
-    for d in os.listdir(TMP):
-      os.rename(os.path.join(TMP,d),os.path.join(TMP,str(dirnum)))
-      dirnum+=1
-    with tarfile.open(f"/tmp/sopt{randstring(32)}.tar.gz", "w:gz") as tar:
-      tar.add(TMP, arcname=os.path.basename(TMP))
-    shutil.rmtree(TMP)
-    print()
-    #ccgpret = p.map(gen_ccg, list(range(ncpu)))
-    #csmithret = p.map(gen_csmith, list(range(ncpu)))
-    #ldrgenret = p.map(gen_ldrgen, list(range(ncpu)))
-
-def bbpe_train(vocab_size):
-  i = 0
-  os.makedirs(f'{TMP}/opt',exist_ok=True)
-  os.makedirs(f'{TMP}/unopt',exist_ok=True)
-  for targz in list(os.listdir(f"{ROOTDIR}/data"))[:2]:
-    print(i)
-    i += 1
-    if not targz.endswith('.tar.gz'):
-      continue
-
-    with tarfile.open(f"{ROOTDIR}/data/{targz}", 'r:gz') as tar:
-      for member in tar.getmembers():
-        if not member.name.endswith('opt.cpp.o'):
-          continue
-
-        file_obj = tar.extractfile(member)
-        content = file_obj.read()
-        subfolder = 'unopt' if 'unopt' in member.name else 'opt'
-        with open(f'{TMP}/{subfolder}/{randstring(32)}.o','wb+') as f:
-          f.write(content)
-
-  paths = [str(x) for x in Path(f"{TMP}/unopt").glob("**/*.o")]
-  tokenizer = tokenizers.ByteLevelBPETokenizer()
-  tokenizer.train(files=paths, vocab_size=vocab_size, min_frequency=2, special_tokens=[])
+CCFLAGS = '-Wall -fcf-protection=none -fno-asynchronous-unwind-tables -fno-unwind-tables -march=znver3 '
 
 
-def sentencepiece_train(vocab_size):
-  os.makedirs(f'{TMP}',exist_ok=True)
-  i = 0
-  with open(f'{TMP}/sopt_opt.txt','wt+') as optf, open(f'{TMP}/sopt_unopt.txt','wt+') as unoptf:
-    for targz in list(os.listdir(f"{ROOTDIR}/data")):
-      print(i)
-      i+=1
-      if not targz.endswith('.tar.gz'):
-        continue
+def gen_compile(index):
+    path = f'{TMP}/{randstring(32)}'
+    os.makedirs(path, exist_ok=False)
+    ret = run(f'{ROOTDIR}/bin/{platform.system()}/yarpgen --std=c++ -o {path}'.split(), stdin=PIPE, stdout=PIPE,
+              stderr=PIPE)
+    clang = f'clang++ -xc++  '
+    unopt_objpath = f'{path}/{index}.unopt.o'
+    opt_objpath = f'{path}/{index}.opt.o'
 
-      with tarfile.open(f"{ROOTDIR}/data/{targz}", 'r:gz') as tar:
-        for member in tar.getmembers():
-          if not member.name.endswith('opt.cpp.o'):
-            continue
+    # Compile unoptimized version
+    clangret = run(f'{clang} -c {path}/func.cpp -o {unopt_objpath} -O0 {CCFLAGS} -include stdint.h'.split(),
+                   stdout=PIPE, stderr=PIPE)
+    stripret = run(f'llvm-strip {unopt_objpath}'.split(), stdout=PIPE, stderr=PIPE)
+    objcopyret = run(
+        f'llvm-objcopy --remove-section .eh_frame --remove-section .note.GNU-stack --remove-section .comment --remove-section .llvm_addrsig {unopt_objpath}'.split(),
+        stdout=PIPE, stderr=PIPE)
 
-          file_obj = tar.extractfile(member)
-          lines = list(chunkify(base64.b64encode(file_obj.read()).decode('ascii'),65000))
-          outf = optf if member.name.endswith('.opt.cpp.o') else unoptf
-          for line in lines:
-            outf.writelines(line + '\n')
+    # Compile optimized version
+    clangret = run(f'{clang} -c {path}/func.cpp -o {opt_objpath} -O3 {CCFLAGS} -include stdint.h'.split(), stdout=PIPE,
+                   stderr=PIPE)
+    stripret = run(f'llvm-strip {opt_objpath}'.split(), stdout=PIPE, stderr=PIPE)
+    objcopyret = run(
+        f'llvm-objcopy --remove-section .eh_frame --remove-section .note.GNU-stack --remove-section .comment --remove-section .llvm_addrsig {opt_objpath}'.split(),
+        stdout=PIPE, stderr=PIPE)
 
-  run(f'spm_train --input={TMP}/sopt_unopt.txt --num_threads=32 --train_extremely_large_corpus=1 --max_sentence_length=10000000 --max_sentencepiece_length=16 --unk_id=0 --bos_id=-1 --eos_id=-1 --pad_id=1 --model_type=unigram --model_prefix=encoder --vocab_size=65000 --character_coverage=1.0'.split())
-  run(f'spm_train --input={TMP}/sopt_opt.txt --num_threads=32 --train_extremely_large_corpus=1 --max_sentence_length=10000000 --max_sentencepiece_length=16 --unk_id=0 --bos_id=-1 --eos_id=-1 --pad_id=1 --model_type=unigram --model_prefix=decoder --vocab_size=65000 --character_coverage=1.0'.split())
+    print(f"Generated {path}")
+    return (unopt_objpath, opt_objpath)
+
+
+def generate_and_save_code(num_progs=10000, output_file='compiler_data.tar.gz'):
+    ncpu = multiprocessing.cpu_count()
+    i = 0
+    with tarfile.open(output_file, "w:gz") as tar:
+        with ProcessPoolExecutor(max_workers=ncpu) as executor:
+            futures = [executor.submit(gen_compile, i) for i in range(num_progs)]
+            for i, future in enumerate(futures):
+                print(f"{i}")
+                unopt_path, opt_path = future.result()
+
+                # Add files to tar
+                tar.add(unopt_path, arcname=f'{i}.unopt.o')
+                tar.add(opt_path, arcname=f'{i}.opt.o')
+
+                # Clean up the generated files
+                os.remove(unopt_path)
+                os.remove(opt_path)
+                shutil.rmtree(os.path.dirname(unopt_path))
+
+    print(f"Generated and saved {num_progs} pairs to {output_file}")
 
 
 if __name__ == '__main__':
-  #generate_code()
-
-  vocab_size = 8192
-  sentencepiece_train(vocab_size)
-  #bbpe_train(vocab_size)
+    generate_and_save_code(num_progs=10000, output_file='compiler_data.tar.gz')
--- ssc.py
+++ ssc.py
@@ -1,211 +1,180 @@
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 import os
+import sys
+import ast
+import csv
+import gzip
 import torch
-import tqdm
-import tarfile
 import random
-import math
+import tqdm
 from x_transformers import XTransformer
-from torch.utils.data import Dataset, DataLoader
-import multiprocessing
-import base64
-import msamp
-from msamp import deepspeed
+import tarfile
 
-if not torch.cuda.is_available():
-  raise Exception("cuda not available")
-torch.set_default_device('cuda')
-#torch.multiprocessing.set_start_method('spawn')
 
-DEVICES = 2
-if DEVICES > 1:
-  STRATEGY = 'deepspeed_stage_3_offload'
-else:
-  STRATEGY = ""
+from util import report_cuda_size, timeit, report_model_size, chunkify
 
-DEVICE = 'cuda'
-ROOTDIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-NUM_TOKENS = 65000+5
-ENC_SEQ_LEN = 2048
-DEC_SEQ_LEN = 2048
-BATCH_SIZE = 1
+ENC_SEQ_LEN = 3072
+DEC_SEQ_LEN = 3072
+BOS = 256
+EOS = 257
+MASK = 258
+DECSTART = 259
+ENCSTART = 260
+PAD = 261
+VOCAB_SIZE=262
+
+
+BATCH_SIZE=1
+WORLD_SIZE = 1
 LEARNING_RATE = 1e-4
-CHECKPOINT_EVERY = 1000
-GENERATE_EVERY = 10000
-NUM_EPOCHS = 10000
+NUM_BATCHES = 10000
+GENERATE_EVERY=10
 
-ENCSTART, ENCEND, DECSTART, DECEND, PAD = [65000, 65001, 65002, 65003, 65004]
+DEVICE='cuda'
 
-# Tokenization functions
-def tokenize(data: bytes):
-  return list(data)
+ROOTDIR = os.path.abspath(os.path.dirname(__file__))
+CHECKPOINT = f"{ROOTDIR}/checkpoint.pt"
+
+def tokenize(inp: bytes):
+  return list(inp)
 
 def detokenize(tokens: [int]):
   ret = []
   for t in tokens:
-    if ENCSTART <= t <= PAD:
-      pass
-    else:
+    if t < 256:
       ret.append(t)
   return bytes(ret)
 
-import sentencepiece as spm
 
-# Load the SentencePiece tokenizers
-enc_sp = spm.SentencePieceProcessor(model_file='encoder.model')
-dec_sp = spm.SentencePieceProcessor(model_file='decoder.model')
+def cycle(targz):
+  i = 0
+  training_data = []
+  while True:
+    unopt_file = targz.extractfile(f'{i}.unopt.o')
+    opt_file = targz.extractfile(f'{i}.opt.o')
+    unopt_bytes = unopt_file.read()
+    opt_bytes = opt_file.read()
 
-# Tokenization functions
-def spm_tokenize(data: bytes, is_encoder=True):
-  if is_encoder:
-    return enc_sp.encode(base64.b64encode(data).decode('ascii'), out_type=int)
-  else:
-    return dec_sp.encode(base64.b64encode(data).decode('ascii'), out_type=int)
-
-def spm_detokenize(tokens: [int], is_encoder=True):
-  if is_encoder:
-    return enc_sp.decode(tokens)
-  else:
-    return dec_sp.decode(tokens)
-
-class XTransformerDataset(torch.utils.data.IterableDataset):
-  def __init__(self, rootdir):
-    super().__init__()
-    self.rootdir = rootdir
-    self.tars = os.listdir(f'{rootdir}/data/')
-    random.shuffle(self.tars)
-
-  def __iter__(self):
-    worker_info = torch.utils.data.get_worker_info()
-    if worker_info is None:  # single-process data loading
-      tar_list = self.tars
-    else:  # in a worker process
-      # split workload
-      per_worker = int(math.ceil(len(self.tars) / float(worker_info.num_workers)))
-      worker_id = worker_info.id
-      start = worker_id * per_worker
-      end = min(start + per_worker, len(self.tars))
-      tar_list = self.tars[start:end]
-
-    for tar in tar_list:
-      if not tar.endswith('.tar.gz'):
-        continue
-
-      with tarfile.open(f"{self.rootdir}/data/{tar}", 'r:gz') as tar_file:
-        entries = []
-        print(f"loading {self.rootdir}/data/{tar}")
-        for member in tar_file.getmembers():
-          if not member.name.endswith('.o'):
-            continue
-
-          if member.name.endswith('.opt.cpp.o'):
-            file_obj = tar_file.extractfile(member)
-            opt_content = file_obj.read()
-          elif member.name.endswith('.unopt.cpp.o'):
-            file_obj = tar_file.extractfile(member)
-            unopt_content = file_obj.read()
-            unopt_tokens = spm_tokenize(unopt_content, True)
-            opt_tokens = spm_tokenize(opt_content, False)
-            if len(unopt_tokens) < ENC_SEQ_LEN and len(opt_tokens) < DEC_SEQ_LEN:
-              entries.append((unopt_tokens, opt_tokens))
-
-        # Sort entries by the sum of unopt and opt token sizes
-        entries.sort(key=lambda x: len(x[0]) + len(x[1]), reverse=True)
-
-        # Fill up the context with as many entries as possible
-        while entries:
-          unopt_batch = [ENCSTART]
-          opt_batch = [DECSTART]
-          mask = [True]
-          curr_enc_len = 1
-          curr_dec_len = 1
-
-          while entries:
-            unopt_tokens, opt_tokens = entries[-1]
-
-            # Check if adding the current entry would overflow either context
-            if curr_enc_len + len(unopt_tokens) + 2 > ENC_SEQ_LEN or curr_dec_len + len(
-              opt_tokens) + 2 > DEC_SEQ_LEN:
-              break
-
-            entries.pop()
-            unopt_batch.extend(unopt_tokens + [ENCEND, ENCSTART])
-            opt_batch.extend(opt_tokens + [DECEND, DECSTART])
-            mask.extend([True] * (len(unopt_tokens) + 2))
-            curr_enc_len += len(unopt_tokens) + 2
-            curr_dec_len += len(opt_tokens) + 2
-
-          unopt_batch = unopt_batch[:-1] + [ENCEND]
-          opt_batch = opt_batch[:-1] + [DECEND]
-          mask = mask[:-1]
-
-          # Pad the sequences
-          unopt_batch.extend([PAD] * (ENC_SEQ_LEN - len(unopt_batch)))
-          opt_batch.extend([PAD] * (DEC_SEQ_LEN - len(opt_batch)))
-          mask.extend([False] * (ENC_SEQ_LEN - len(mask)))
-
-          yield torch.tensor(unopt_batch, dtype=torch.long), torch.tensor(opt_batch,
-                                          dtype=torch.long), torch.tensor(
-            mask, dtype=torch.bool)
+    unopt_tokens = tokenize(unopt_bytes)
+    opt_tokens = tokenize(opt_bytes)
+    #print(f"len unopt tokens {len(unopt_tokens)} len opt tokens {len(opt_tokens)} len unopt {len(unopt)} len opt {len(opt)}")
+    if len(unopt_tokens) >= ENC_SEQ_LEN or len(opt_tokens) >= DEC_SEQ_LEN:
+      continue
+    opt_tokens.insert(0, DECSTART)
+    mask = [True] * len(unopt_tokens)
+    mask.extend([False] * (ENC_SEQ_LEN - len(unopt_tokens)))
+    unopt_tokens.extend([PAD] * (ENC_SEQ_LEN - len(unopt_tokens)))
+    opt_tokens.extend([PAD] * (DEC_SEQ_LEN - len(opt_tokens)))
+    training_data.append([unopt_tokens, opt_tokens, mask])
+    if len(training_data) == BATCH_SIZE:
+      batch = training_data[:BATCH_SIZE]
+      training_data = training_data[BATCH_SIZE:]
+      mysrc = torch.tensor(list(x[0] for x in batch)).long().to(DEVICE)
+      mytgt = torch.tensor(list(x[1] for x in batch)).long().to(DEVICE)
+      mysrc_mask = torch.tensor(list(x[2] for x in batch)).bool().to(DEVICE)
+      yield mysrc, mysrc_mask, mytgt
+  print("we should never get here")
 
 
-class XTransformerModel(torch.nn.Module):
-  def __init__(self):
-    super().__init__()
-    self.model = XTransformer(
-      dim=1024,
-      pad_value=PAD,
-      tie_token_emb=True,
-      enc_attn_flash=True,
-      dec_attn_flash=True,
-      return_tgt_loss=True,
-      enc_num_tokens=NUM_TOKENS,
-      enc_depth=10,
-      enc_heads=10,
-      enc_max_seq_len=ENC_SEQ_LEN,
-      dec_num_tokens=NUM_TOKENS,
-      dec_depth=10,
-      dec_heads=10,
-      dec_max_seq_len=DEC_SEQ_LEN,
-    )
 
-  def forward(self, src, tgt, mask):
-    return self.model(src, tgt, mask=mask)
+def save_checkpoint(model,  optim, loss, scaler, scheduler):
+  if DEVICE == 'cuda':
+    torch.save({
+      'model_state_dict': model.state_dict(),
+      'optimizer_state_dict': optim.state_dict(),
+      'loss': loss.item(),
+      'scaler': scaler.state_dict(),
+      'scheduler': scheduler.state_dict()},
+      CHECKPOINT)
 
-  def generate(self, src, start_tokens, seq_len, mask):
-    return self.model.generate(src, start_tokens, seq_len, mask=mask)
+def load_checkpoint(model, optim, loss):
+  if os.path.exists(CHECKPOINT):
+    print(f"loading {CHECKPOINT}")
+    checkpoint = torch.load(CHECKPOINT)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optim.load_state_dict(checkpoint['optimizer_state_dict'])
+    loss = checkpoint['loss']
+  return model, optim,  loss
+
+@timeit
+def train(rank):
+
+  if WORLD_SIZE > 1:
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    torch.distributed.init_process_group(backend='nccl', rank=rank,world_size=WORLD_SIZE)
+    torch.cuda.set_device(rank)
+
+  model = XTransformer(
+    dim=768,
+    pad_value=PAD,
+    tie_token_emb=True,
+    enc_attn_flash=True,
+    dec_attn_flash=True,
+    return_tgt_loss=True,
+    enc_num_tokens=VOCAB_SIZE,
+    enc_depth=8,
+    enc_heads=8,
+    enc_max_seq_len=ENC_SEQ_LEN,
+    dec_num_tokens=VOCAB_SIZE,
+    dec_depth=8,
+    dec_heads=8,
+    dec_max_seq_len=DEC_SEQ_LEN).cuda()
 
 
-def train(model, dataloader, optimizer, num_epochs):
-  model.train()
-  for epoch in range(num_epochs):
-    i = 0
-    for batch in dataloader:
-      print(f"batch {i}")
-      i+=1
-      src, tgt, mask = batch
-      src, tgt, mask = src.to(DEVICE), tgt.to(DEVICE), mask.to(DEVICE)
+  report_model_size(model)
+  optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+  if DEVICE == 'cuda':
+    scaler = torch.cuda.amp.GradScaler()
+  scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optim,T_0=100)
+  model, optim, loss = load_checkpoint(model, optim, 0)
 
-      optimizer.zero_grad()
-      loss = model(src, tgt, mask)
-      loss.backward()
-      optimizer.step()
+  targz = tarfile.open(f'/{ROOTDIR}/compiler_data.tar.gz','r:gz')
 
+  for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
+    model.train()
+    optim.zero_grad()
 
-    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+    src, src_mask, tgt = next(cycle(targz))
+    with torch.cuda.amp.autocast(dtype=torch.bfloat16):
+      loss = model(src, tgt, mask=src_mask)
+    scaler.scale(loss).backward()
+    scaler.step(optim)
+    scaler.update()
 
+    scheduler.step(i/NUM_BATCHES)
+    print(f'{i}: {loss.item()}')
+
+    if i == 0 and DEVICE == 'cuda':
+      report_cuda_size()
+    if i % GENERATE_EVERY == 0:
+      with FSDP.summon_full_params(model, writeback=False, recurse=False):
+        #if i > 0:
+        #  save_checkpoint(model,  optim, loss, scaler, scheduler)
+        model.eval()
+        src, src_mask, tgt = next(cycle(targz))
+        #src, src_mask, tgt  = src[:1], src_mask[:1], tgt[:1]
+        start_tokens = torch.tensor([DECSTART]).to(DEVICE)
+        #getting an off by one error when trying to pass the mask here because ???
+        #so skip for now
+        sample = model.generate(src, start_tokens, DEC_SEQ_LEN)
+
+        print_stmt = f'\nRANK: {rank} start\n'
+        print_stmt += f"\ninput tokenized:  \n{detokenize(src.tolist()[0])} \n"
+        print_stmt += f"\npredicted detokenized:  \n{detokenize(sample.tolist())}\n"
+        print_stmt += f"\nactual detokenized:     \n{detokenize(tgt.tolist()[0])}\n"
+        print_stmt += f'\nRANK: {rank} end\n'
+        print(print_stmt)
+
+  if WORLD_SIZE > 1:
+    torch.distributed.destroy_process_group()
 
 def main():
-  dataset = XTransformerDataset(ROOTDIR)
-  dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, num_workers=0)
-
-  model = XTransformerModel().cuda()
-  optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
-  model, optimizer = msamp.initialize(model, optimizer, opt_level="O2")
-
-  train(model, dataloader, optimizer, NUM_EPOCHS)
-
+  print(f'spawning {WORLD_SIZE} processes(s)')
+  if WORLD_SIZE == 1:
+    train(0)
+  else:
+    torch.multiprocessing.spawn(train, args=(), nprocs=WORLD_SIZE,join=True)
 
 if __name__ == '__main__':
-  torch.set_float32_matmul_precision('medium')
   main()
